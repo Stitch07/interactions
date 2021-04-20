c9842cd094cfca214392e53466b2c1a0afc92a86
Vlad Frangu <kingdgrizzle@gmail.com>
2021-04-20 19:47:01 +0300

    chore: more... progress I suppose Yknow building this to give it a nice UX is harder than I thought

--- src/lib/structures/interactions/SlashCommandArgs.ts
+++ src/lib/structures/interactions/SlashCommandArgs.ts
@@ -1,1 +1,37 @@
-export class SlashCommandArgs {}
+import type { Command } from '@sapphire/framework';
+import type { APIApplicationCommandInteractionData } from 'discord-api-types/v8';
+import type { SlashCommandContext, SlashCommandInteraction } from './SlashCommandInteraction';
+
+export class SlashCommandArgs {
+	/**
+	 * The original interaction that triggered the command
+	 */
+	public readonly interaction: SlashCommandInteraction;
+
+	/**
+	 * The command that is being run
+	 */
+	public readonly command: Command;
+
+	/**
+	 * The context of the command being run
+	 */
+	public readonly context: SlashCommandContext;
+
+	/**
+	 * The internal data that is processed
+	 */
+	protected readonly rawData: APIApplicationCommandInteractionData;
+
+	public constructor(
+		interaction: SlashCommandInteraction,
+		command: Command,
+		context: SlashCommandContext,
+		data: APIApplicationCommandInteractionData
+	) {
+		this.interaction = interaction;
+		this.command = command;
+		this.context = context;
+		this.rawData = data;
+	}
+}
--- src/lib/structures/interactions/SlashCommandInteraction.ts
+++ src/lib/structures/interactions/SlashCommandInteraction.ts
@@ -1,7 +1,138 @@
-import type { APIInteraction } from 'discord-api-types/v8';
+import { isApplicationCommandGuildInteraction } from 'discord-api-types/utils/v8';
+import type {
+	APIApplicationCommandInteraction,
+	APIApplicationCommandInteractionData,
+	APIGuildInteraction,
+	APIUser,
+	InteractionType,
+	Snowflake
+} from 'discord-api-types/v8';
+import { Client, DMChannel, Guild, GuildMember, NewsChannel, TextChannel, User, WebhookClient } from 'discord.js';
+import { SlashCommandArgs } from './SlashCommandArgs';
 
-export class SlashCommandInteraction {}
+export class SlashCommandInteraction {
+	/**
+	 * The ID of the interaction
+	 */
+	public readonly id: Snowflake;
+
+	/**
+	 * The application ID this interaction is for
+	 */
+	public readonly applicationID: Snowflake;
+
+	/**
+	 * The type of the interaction - will always be APPLICATION_COMMAND
+	 */
+	public readonly type: InteractionType;
+
+	/**
+	 * The channel ID this interaction was sent in
+	 */
+	public readonly channelID: Snowflake;
+
+	/**
+	 * A continuation token for this interaction
+	 *
+	 * This is used when creating the Followup Webhook client
+	 */
+	public readonly token: string;
+
+	/**
+	 * The guild ID this interaction was sent in, if it was sent in a guild
+	 */
+	public readonly guildID: Snowflake | null;
+
+	/**
+	 * The author of this interaction
+	 */
+	public readonly rawUser: APIUser;
+
+	/**
+	 * The raw member that created this interaction, if the interaction was sent in a guild
+	 */
+	public readonly rawMember: APIGuildInteraction['member'] | null;
+
+	/**
+	 * A webhook client that can be used for sending followup responses to the interaction
+	 *
+	 * Note: This client becomes invalid after **15 minutes**, due to the token expiry
+	 */
+	public readonly followupClient: WebhookClient;
+
+	/**
+	 * The raw interaction data received with this interaction.
+	 *
+	 * `SlashCommandArgs` is constructed from this.
+	 */
+	public readonly rawInteractionData: APIApplicationCommandInteractionData;
+
+	/**
+	 * The command being ran in this interaction
+	 */
+	public readonly commandData: SlashCommandData;
+
+	public constructor(public client: Client, interaction: APIApplicationCommandInteraction) {
+		this.id = interaction.id;
+		this.applicationID = interaction.application_id;
+		this.type = interaction.type;
+		this.rawInteractionData = interaction.data;
+		this.channelID = interaction.channel_id;
+		this.token = interaction.token;
+		this.guildID = isApplicationCommandGuildInteraction(interaction) ? interaction.guild_id : null;
+		this.rawUser = isApplicationCommandGuildInteraction(interaction) ? interaction.member.user : interaction.user;
+		this.rawMember = isApplicationCommandGuildInteraction(interaction) ? interaction.member : null;
+
+		this.followupClient = new WebhookClient(interaction.application_id, interaction.token);
+
+		this.commandData = { id: interaction.data.id, name: interaction.data.name };
+	}
+
+	/**
+	 * Gets the user that created this interaction
+	 * @returns The user, if present
+	 */
+	public getUser(): User | null {
+		return this.client.users.add(this.rawUser, false);
+	}
+
+	/**
+	 * Gets the channel in which this interaction was created
+	 * @returns The channel, if present
+	 */
+	public getChannel(): DMChannel | TextChannel | NewsChannel | null {
+		return (this.client.channels.cache.get(this.channelID) as DMChannel | TextChannel | NewsChannel) ?? null;
+	}
+
+	/**
+	 * Gets the guild in which this interaction was created
+	 * @returns The guild, if present
+	 */
+	public getGuild(): Guild | null {
+		return this.client.guilds.cache.get(this.guildID!) ?? null;
+	}
+
+	/**
+	 * Gets the member that created this interaction in the guild
+	 * @returns The member, if the interaction was created in a guild
+	 */
+	public getMember(): GuildMember | null {
+		const guild = this.getGuild();
+		return guild?.members.add(this.rawMember, false, { id: this.rawUser.id, extras: [guild] }) ?? null;
+	}
+}
 
 export interface SlashCommandContext extends Record<PropertyKey, unknown> {
-	rawData: APIInteraction;
+	rawData: APIApplicationCommandInteraction;
+}
+
+export interface SlashCommandData {
+	/**
+	 * The ID of the slash command that was executed
+	 */
+	id: Snowflake;
+	/**
+	 * The name of the slash command that was executed
+	 */
+	name: string;
 }
