2ea3ffd405df1c4309cd8163a962b9aa838fe669
Jeroen Claassens <support@favware.tech>
2021-05-08 22:13:19 +0200

    test: fix tests

--- tests/SlashCommandAssertions.test.ts
+++ tests/SlashCommandAssertions.test.ts
@@ -1,61 +1,61 @@
-import { slashCommandAssertions } from '../src/index';
+import { SlashCommandAssertions } from '../src/index';
 
 const largeArray = Array.from({ length: 26 }, () => 1 as any);
 
 describe('Slash Command Assertions tests', () => {
 	test('GIVEN valid name THEN does not throw error', () => {
-		expect(() => slashCommandAssertions.validateName('ping')).not.toThrowError();
+		expect(() => SlashCommandAssertions.validateName('ping')).not.toThrowError();
 	});
 
 	test('GIVEN invalid name THEN throw error', () => {
 		// @ts-expect-error Testing validation with invalid data
-		expect(() => slashCommandAssertions.validateName(null)).toThrowError();
+		expect(() => SlashCommandAssertions.validateName(null)).toThrowError();
 
 		// Too short of a name
-		expect(() => slashCommandAssertions.validateName('')).toThrowError();
+		expect(() => SlashCommandAssertions.validateName('')).toThrowError();
 
 		// Invalid characters used
-		expect(() => slashCommandAssertions.validateName('ABC123$%^&')).toThrowError();
+		expect(() => SlashCommandAssertions.validateName('ABC123$%^&')).toThrowError();
 
 		// Too long of a name
-		expect(() => slashCommandAssertions.validateName('qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm')).toThrowError();
+		expect(() => SlashCommandAssertions.validateName('qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm')).toThrowError();
 	});
 
 	test('GIVEN valid description THEN does not throw error', () => {
-		expect(() => slashCommandAssertions.validateDescription('This is an OwO moment fur sure!~')).not.toThrowError();
+		expect(() => SlashCommandAssertions.validateDescription('This is an OwO moment fur sure!~')).not.toThrowError();
 	});
 
 	test('GIVEN invalid description THEN throw error', () => {
 		// @ts-expect-error Testing validation with invalid data
-		expect(() => slashCommandAssertions.validateDescription(null)).toThrowError();
+		expect(() => SlashCommandAssertions.validateDescription(null)).toThrowError();
 
 		// Too short of a description
-		expect(() => slashCommandAssertions.validateDescription('')).toThrowError();
+		expect(() => SlashCommandAssertions.validateDescription('')).toThrowError();
 
 		// Too long of a description
 		expect(() =>
-			slashCommandAssertions.validateDescription(
+			SlashCommandAssertions.validateDescription(
 				'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam autem libero expedita vitae accusamus nostrum ipsam tempore repudiandae deserunt ipsum facilis, velit fugiat facere accusantium, explicabo corporis aliquam non quos.'
 			)
 		).toThrowError();
 	});
 
 	test('GIVEN valid array of options or choices THEN does not throw error', () => {
-		expect(() => slashCommandAssertions.validateMaxOptionsLength([])).not.toThrowError();
+		expect(() => SlashCommandAssertions.validateMaxOptionsLength([])).not.toThrowError();
 
-		expect(() => slashCommandAssertions.validateMaxChoicesLength([])).not.toThrowError();
+		expect(() => SlashCommandAssertions.validateMaxChoicesLength([])).not.toThrowError();
 	});
 
 	test('GIVEN invalid options or choices THEN throw error', () => {
 		// @ts-expect-error Testing validation with invalid data
-		expect(() => slashCommandAssertions.validateMaxOptionsLength(null)).toThrowError();
+		expect(() => SlashCommandAssertions.validateMaxOptionsLength(null)).toThrowError();
 
 		// @ts-expect-error Testing validation with invalid data
-		expect(() => slashCommandAssertions.validateMaxChoicesLength(null)).toThrowError();
+		expect(() => SlashCommandAssertions.validateMaxChoicesLength(null)).toThrowError();
 
 		// Given an array that's too big
-		expect(() => slashCommandAssertions.validateMaxOptionsLength(largeArray)).toThrowError();
+		expect(() => SlashCommandAssertions.validateMaxOptionsLength(largeArray)).toThrowError();
 
-		expect(() => slashCommandAssertions.validateMaxChoicesLength(largeArray)).toThrowError();
+		expect(() => SlashCommandAssertions.validateMaxChoicesLength(largeArray)).toThrowError();
 	});
 });
